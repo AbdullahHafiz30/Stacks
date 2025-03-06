f2862cfb2c58601da94c2cb2a42896a04a54d27d
AbdullahHafiz30 <ABDULLAH_MH30@outlook.com>
2025-03-06 11:47:38 +0300

    Initial Commit

---
+++ stacks/stacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  stacksApp.swift
+//  stacks
+//
+//  Created by عبدالله حافظ on 06/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct stacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
